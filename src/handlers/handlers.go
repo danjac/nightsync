785ae666cfde31b83bf854a257eec6b3e418b5f6
Dan Jacob <danjac354@gmail.com>
2016-02-20 20:01:43 +0200

    Refactoring

--- src/handlers/handlers.go
+++ src/handlers/handlers.go
@@ -28,8 +28,14 @@ import (
 )
 
 var (
-	verifyKey *rsa.PublicKey
-	signKey   *rsa.PrivateKey
+	verifyKey   *rsa.PublicKey
+	signKey     *rsa.PrivateKey
+	authOptions *yelp.AuthOptions
+)
+
+var (
+	db = initDB()
+	h  = initHub()
 )
 
 const (
@@ -60,6 +66,23 @@ func init() {
 
 	verifyKey, err = jwt.ParseRSAPublicKeyFromPEM(verifyBytes)
 
+	authOptions = &yelp.AuthOptions{
+		ConsumerKey:       os.Getenv("YELP_CONSUMER_KEY"),
+		ConsumerSecret:    os.Getenv("YELP_CONSUMER_SECRET"),
+		AccessToken:       os.Getenv("YELP_ACCESS_TOKEN"),
+		AccessTokenSecret: os.Getenv("YELP_ACCESS_TOKEN_SECRET"),
+	}
+
+	twitterKey := os.Getenv("TWITTER_KEY")
+	twitterSecret := os.Getenv("TWITTER_SECRET")
+
+	goth.UseProviders(
+		twitter.New(twitterKey, twitterSecret,
+			"http://localhost:4000/auth/callback/?provider=twitter",
+		),
+	)
+
+	gothic.Store = sessions.NewCookieStore([]byte(os.Getenv("SECRET_KEY")))
 }
 
 func fatal(err error) {
@@ -76,11 +99,17 @@ func (e httpError) Error() string {
 	return http.StatusText(e.status)
 }
 
-func abortWithStatus(w http.ResponseWriter, status int) {
-	abortWithError(w, httpError{status})
+func newHTTPError(status int) error {
+	return httpError{status}
 }
 
-func abortWithError(w http.ResponseWriter, err error) {
+func renderJSON(w http.ResponseWriter, status int, payload interface{}) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(payload)
+}
+
+func handleError(w http.ResponseWriter, err error) {
 	if httpError, ok := err.(httpError); ok {
 		http.Error(w, httpError.Error(), httpError.status)
 		return
@@ -89,11 +118,6 @@ func abortWithError(w http.ResponseWriter, err error) {
 	log.Println(err)
 	http.Error(w, err.Error(), http.StatusInternalServerError)
 }
-
-var (
-	db = initDB()
-	h  = initHub()
-)
 
 // grabs user ID from context
 func getUserID(r *http.Request) string {
@@ -126,7 +150,7 @@ func authenticate(w http.ResponseWriter, r *http.Request) error {
 		//check if timeout etc
 		log.Println("JWTERROR", err)
 		// if a timeout throw a 401, so we can prompt re-login
-		return httpError{http.StatusUnauthorized}
+		return newHTTPError(http.StatusUnauthorized)
 	}
 	if t.Valid {
 		context.Set(r, userContextKey, t.Claims["userid"])
@@ -136,30 +160,107 @@ func authenticate(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-// authentication middleware: decodes JWT token
-func authenticator(next http.Handler) http.Handler {
-	// token will either be in header or query string (e.g. sockets)
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if err := authenticate(w, r); err != nil {
-			abortWithError(w, err)
-			return
+type handlerFunc func(http.ResponseWriter, *http.Request) error
+
+type handler struct {
+	H handlerFunc
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := authenticate(w, r); err != nil {
+		handleError(w, err)
+		return
+	}
+	if err := h.H(w, r); err != nil {
+		handleError(w, err)
+	}
+}
+
+func newHandler(h handlerFunc) http.Handler {
+	return handler{h}
+}
+
+func socket(w http.ResponseWriter, r *http.Request) error {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return err
+	}
+	cn := &conn{
+		send:   make(chan *Message),
+		ws:     ws,
+		userID: getUserID(r),
+		h:      h}
+	h.register <- cn
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go cn.write(&wg)
+	go cn.read(&wg)
+	wg.Wait()
+	return nil
+
+}
+
+func authCallback(w http.ResponseWriter, r *http.Request) error {
+	creds, err := gothic.CompleteUserAuth(w, r)
+	if err != nil {
+		return err
+	}
+	expires := time.Now().Add(time.Hour * 24)
+	userID := fmt.Sprintf("%s:%s", creds.Provider, creds.UserID)
+
+	token := jwt.New(jwt.SigningMethodRS256)
+	token.Claims["userid"] = userID
+	token.Claims["exp"] = expires.Unix()
+	tokenStr, err := token.SignedString(signKey)
+	if err != nil {
+		return err
+	}
+	// the client can now just read the token from the query string
+	url := fmt.Sprintf("%s?jwt-token=%s", clientURL, tokenStr)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+	return nil
+}
+
+func searchLocation(w http.ResponseWriter, r *http.Request) error {
+	location := r.URL.Query().Get("location")
+	if location == "" {
+		return newHTTPError(http.StatusBadRequest)
+	}
+
+	searchOptions := yelp.SearchOptions{
+		LocationOptions: &yelp.LocationOptions{
+			Location: location,
+		},
+		GeneralOptions: &yelp.GeneralOptions{
+			CategoryFilter: "bars",
+		},
+	}
+
+	client := yelp.New(authOptions, nil)
+	result, err := client.DoSearch(searchOptions)
+	if err != nil {
+		return err
+	}
+
+	bars := make([]Bar, len(result.Businesses))
+	userID := getUserID(r)
+
+	for i, biz := range result.Businesses {
+		bars[i] = Bar{
+			biz.ID,
+			biz.ImageURL,
+			biz.Name,
+			biz.SnippetText,
+			db.getTotal(biz.ID),
+			db.isGoing(biz.ID, userID),
 		}
-		next.ServeHTTP(w, r)
-	})
+	}
+	return renderJSON(w, http.StatusOK, bars)
 }
 
 // Run runs the application.
 func Run(host string) {
-
-	twitterKey := os.Getenv("TWITTER_KEY")
-	twitterSecret := os.Getenv("TWITTER_SECRET")
-
-	goth.UseProviders(
-		twitter.New(twitterKey, twitterSecret,
-			"http://localhost:4000/auth/callback/?provider=twitter",
-		),
-	)
-	gothic.Store = sessions.NewCookieStore([]byte(os.Getenv("SECRET_KEY")))
 
 	cors := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
@@ -168,110 +269,24 @@ func Run(host string) {
 		Debug:            false,
 	})
 
-	authOptions := &yelp.AuthOptions{
-		ConsumerKey:       os.Getenv("YELP_CONSUMER_KEY"),
-		ConsumerSecret:    os.Getenv("YELP_CONSUMER_SECRET"),
-		AccessToken:       os.Getenv("YELP_ACCESS_TOKEN"),
-		AccessTokenSecret: os.Getenv("YELP_ACCESS_TOKEN_SECRET"),
-	}
-
 	router := mux.NewRouter()
 
-	router.HandleFunc("/search/", func(w http.ResponseWriter, r *http.Request) {
-
-		location := r.URL.Query().Get("location")
-		if location == "" {
-			abortWithStatus(w, http.StatusBadRequest)
-			return
-		}
-
-		searchOptions := yelp.SearchOptions{
-			LocationOptions: &yelp.LocationOptions{
-				Location: location,
-			},
-			GeneralOptions: &yelp.GeneralOptions{
-				CategoryFilter: "bars",
-			},
-		}
-
-		client := yelp.New(authOptions, nil)
-		result, err := client.DoSearch(searchOptions)
-		if err != nil {
-			abortWithError(w, err)
-			return
-		}
-
-		bars := make([]Bar, len(result.Businesses))
-		userID := getUserID(r)
-
-		for i, biz := range result.Businesses {
-			bars[i] = Bar{
-				biz.ID,
-				biz.ImageURL,
-				biz.Name,
-				biz.SnippetText,
-				db.getTotal(biz.ID),
-				db.isGoing(biz.ID, userID),
-			}
-		}
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(bars)
-	})
-
-	router.HandleFunc("/ws/", func(w http.ResponseWriter, r *http.Request) {
-		ws, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			abortWithError(w, err)
-			return
-		}
-		cn := &conn{
-			send:   make(chan *Message),
-			ws:     ws,
-			userID: getUserID(r),
-			h:      h}
-		h.register <- cn
-
-		var wg sync.WaitGroup
-		wg.Add(2)
-		go cn.write(&wg)
-		go cn.read(&wg)
-		wg.Wait()
-	})
+	router.Handle("/search/", newHandler(searchLocation)).Methods("GET")
+	router.Handle("/ws/", newHandler(socket)).Methods("GET")
 
 	auth := router.PathPrefix("/auth").Subrouter()
 
+	// oauth provider callback
+	auth.Handle("/callback/", newHandler(authCallback))
+
 	// redirects to provider
 	auth.HandleFunc("/redirect/", gothic.BeginAuthHandler)
-
-	// oauth provider callback
-	auth.HandleFunc("/callback/", func(w http.ResponseWriter, r *http.Request) {
-
-		creds, err := gothic.CompleteUserAuth(w, r)
-		if err != nil {
-			abortWithError(w, err)
-			return
-		}
-		expires := time.Now().Add(time.Hour * 24)
-		userID := fmt.Sprintf("%s:%s", creds.Provider, creds.UserID)
-
-		token := jwt.New(jwt.SigningMethodRS256)
-		token.Claims["userid"] = userID
-		token.Claims["exp"] = expires.Unix()
-		tokenStr, err := token.SignedString(signKey)
-		if err != nil {
-			abortWithError(w, err)
-			return
-		}
-		// the client can now just read the token from the query string
-		url := fmt.Sprintf("%s?jwt-token=%s", clientURL, tokenStr)
-		http.Redirect(w, r, url, http.StatusTemporaryRedirect)
-	})
 
 	// kickoff the connection hub
 
 	go h.run()
 
-	chain := alice.New(cors.Handler, authenticator).Then(router)
+	chain := alice.New(cors.Handler).Then(router)
 
 	if err := http.ListenAndServe(host, chain); err != nil {
 		log.Fatal(err)
